5db09f9155fda366f30b42b321d5f7dc2d56c6f7
w.dev <fznix.we@gmail.com>
2025-04-03 20:38:37 +0100

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+set fish_greeting ""
+oh-my-posh init fish --config $HOME/.poshthemes/montys.omp.json | source
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
