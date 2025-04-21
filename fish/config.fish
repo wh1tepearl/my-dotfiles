dfd1580908ddd719227918099e168993f8957f54
w.dev <fznix.we@gmail.com>
2025-04-21 15:18:31 +0100

    Delete fish directory

--- fish/config.fish
+++
@@ -1,5 +0,0 @@
-set fish_greeting ""
-oh-my-posh init fish --config $HOME/.poshthemes/montys.omp.json | source
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
